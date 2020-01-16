14f23dbdc06ee5e7959b3b8e77ec278268fd7e92
Mateus Miranda <32250130+mateusabe@users.noreply.github.com>
2020-01-16 00:42:19 -0300

    Atualization #3

--- js/floor.js
+++ js/floor.js
@@ -2,6 +2,8 @@ var checkboxes = document.getElementsByClassName('cbox');
 var options = document.getElementById("floor_select");
 
 function toRadio(index) {
+    if(options.disabled == true)
+        options.disabled = false;
     for(var i=0; i<checkboxes.length; i++) {
         if(!checkboxes[i].checked && i == index)
             break;
@@ -12,26 +14,37 @@ function toRadio(index) {
 
 function toRadioFloor(index) {
     if(index == 0){
-        options.options[1].disabled = false;
-        if(!options.options[2].disabled)
-            options.options[2].disabled = true;
-        if(!options.options[3].disabled)
-            options.options[3].disabled = true;
+        if(options.length == 4)
+            remSel(3);
+        if(options.length == 3)
+            remSel(2);
     }
     else if(index == 1){
-        options.options[2].disabled = false;
-        if(!options.options[3].disabled)
-            options.options[3].disabled = true;
-        if(options.options[2].disabled)
-            options.options[2].disabled = false;
-        if(options.options[1].disabled)
-            options.options[1].disabled = false;
+        if(options.length == 4)
+            remSel(3);
+        if(options.length < 2)
+            addSel(1);
+        if(options.length < 3)
+            addSel(2);
     }
     else if(index >= 2){
-        options.options[3].disabled = false;
-        if(options.options[2].disabled)
-            options.options[2].disabled = false;
-        if(options.options[1].disabled)
-            options.options[1].disabled = false;
+        if(options.length < 2)
+            addSel(1);
+        if(options.length < 3)
+            addSel(2);
+        if(options.length < 4)
+            addSel(3);
     }    
+}
+
+function remSel(index){
+ $("#floor_select option[value='" + index + "']").remove();
+}
+
+function addSel(index){
+    var fill;
+    if(index==2)
+        fill = "2 andares";
+    else fill = "3 andares";
+    $("#floor_select").append('<option value="' + index + '">' + fill + '</option>');
 }
